5750edc641f40a1006b55d0b00932fc01d13b609
selwyn25landayan <133663178+selwyn25landayan@users.noreply.github.com>
2023-05-16 00:20:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,11 +22,11 @@ def main():
         return model
     
     def import_and_predict(image_data, model):
-        size=(128,128)
+        size=(224,224)
         image = ImageOps.fit(image_data,size, Image.LANCZOS)
         image = np.asarray(image)
         image = image / 255.0
-        img_reshape = np.reshape(image, (1, 128, 128, 3))
+        img_reshape = np.reshape(image, (1, 224, 224, 3))
         prediction = model.predict(img_reshape)
         return prediction
 
